2d29bc805977a15d85d984bdcee5db738f3a6cd7
Blackraven93 <reblackraven@gmail.com>
2021-12-13 10:22:48 +0900

    Feat: create ToDoList Component

---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+import { DefaultTheme } from "styled-components";
+
+
+export const darkTheme: DefaultTheme = {
+    bgColor: "#222f3e",
+    textColor: "white",
+    accentColor: "#01a3a4",
+    cardBgColor: "transparent",
+};
